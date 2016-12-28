6dda6a7bbfaf769b6f37d09ba5c56e8b291264a8
ADE\vpillai <vspillai@ualr.edu>
2016-12-28 15:26:45 -0600

    Initial COmmit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            Movie random = new Movie() { Id=1, Name="Interstellar" };
+            return View(random);
+        }
+    }
+}
